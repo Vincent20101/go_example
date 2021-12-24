b70284b76ee8fce9e4c507b7b78b49b71cd4b8de
linhuanbo <963335385@qq.com>
2021-12-24 16:38:12 +0800

    fix: hamc_sha256
    Signed-off-by: linhuanbo <963335385@qq.com>

--- filetest/hmac_sha256.go
+++ filetest/hmac_sha256.go
@@ -4,29 +4,30 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
-	"encoding/hex"
 	"fmt"
 	"net/url"
 )
 
 const (
-	message = "hello world!"
-	secret  = "0933e54e76b24731a2d84b6b463ec04c"
+	secret  = "W0MwRTI4MjQyRUE1NUM1Qjc0RDIxMEQ5QUZBQzhFQTA2REIzQ0EzOTJd"
 )
 
-func ComputeHmacSha256(message string, secret string) string {
+func HmacSha256(message string, secret string) string {
 	key := []byte(secret)
 	h := hmac.New(sha256.New, key)
 	h.Write([]byte(message))
-	//	fmt.Println(h.Sum(nil))
-	sha := hex.EncodeToString(h.Sum(nil))
-	//	fmt.Println(sha)
-
-	//	hex.EncodeToString(h.Sum(nil))
-	return base64.StdEncoding.EncodeToString([]byte(sha))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
 
 func main() {
+	//fmt.Println(time.Now().UTC().Format("2006-01-02T15:04:05Z"))
+	message :=""
+	methon := "GET"
+	IP := "10.1.240.6"
+	Path:="/"
+	items := "AWSAccessKeyId=A2A598647C54B13B2078&Action=DescribeAvailabilityZones&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2021-12-24T08%3A05%3A35.000Z&Version=latest"
 
-	fmt.Println(url.QueryEscape("www.baidu.com?a=1&b=1&" + ComputeHmacSha256(message, secret)))
+	message = methon+"\n"+ IP+"\n"+Path+"\n"+items
+	fmt.Println(HmacSha256(message, secret))
+	fmt.Println(url.QueryEscape(HmacSha256(message, secret)))
 }
