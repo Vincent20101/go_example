8b93358e75091083da1ebd8d0a020b81f599682b
linhuanbo <963335385@qq.com>
2021-12-27 16:04:24 +0800

    test: hmac_sha256
    Signed-off-by: linhuanbo <963335385@qq.com>

--- filetest/hmac_sha256.go
+++ filetest/hmac_sha256.go
@@ -6,10 +6,17 @@ import (
 	"encoding/base64"
 	"fmt"
 	"net/url"
+	"sort"
+	"strings"
+	"time"
 )
 
 const (
-	secret  = "W0MwRTI4MjQyRUE1NUM1Qjc0RDIxMEQ5QUZBQzhFQTA2REIzQ0EzOTJd"
+	SECRET  = "W0MwRTI4MjQyRUE1NUM1Qjc0RDIxMEQ5QUZBQzhFQTA2REIzQ0EzOTJd"
+	AWS_ACCESS_KEY_ID = "A2A598647C54B13B2078"
+	SIGNATURE_METHOD = "HmacSHA256"
+	SIGNATURE_VERSION = "2"
+	VERSION = "latest"
 )
 
 func HmacSha256(message string, secret string) string {
@@ -20,14 +27,24 @@ func HmacSha256(message string, secret string) string {
 }
 
 func main() {
-	//fmt.Println(time.Now().UTC().Format("2006-01-02T15:04:05Z"))
 	message :=""
+
 	methon := "GET"
 	IP := "10.1.240.6"
 	Path:="/"
-	items := "AWSAccessKeyId=A2A598647C54B13B2078&Action=DescribeAvailabilityZones&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2021-12-24T08%3A05%3A35.000Z&Version=latest"
+	itemSlice := sort.StringSlice{
+		"SignatureMethod=" + SIGNATURE_METHOD,
+		"Action=DescribeInstances",
+		"Timestamp="+url.QueryEscape(time.Now().UTC().Format("2006-01-02T15:04:05")+".000Z"),
+		"SignatureVersion=" + SIGNATURE_VERSION,
+		"AWSAccessKeyId=" + AWS_ACCESS_KEY_ID,
+		"Version=" + VERSION}
 
+	sort.Sort(itemSlice)
+
+	items := strings.Join(itemSlice, "&")
 	message = methon+"\n"+ IP+"\n"+Path+"\n"+items
-	fmt.Println(HmacSha256(message, secret))
-	fmt.Println(url.QueryEscape(HmacSha256(message, secret)))
+
+	fmt.Println(HmacSha256(message, SECRET))
+	fmt.Println(url.QueryEscape(HmacSha256(message, SECRET)))
 }
