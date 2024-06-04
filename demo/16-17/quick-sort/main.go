bada300ed9f7f3f9fe7845e6f414cf7e5bbdd2bc
lee <you@example.com>
2024-06-04 18:39:14 +0800

    update

--- demo/16-17/quick-sort/main.go
+++ demo/16-17/quick-sort/main.go
@@ -19,6 +19,41 @@ func QuickSort(array []int, begin, end int) {
 }
 
 // 切分函数，并返回切分元素的下标
+/**
+左指针扫描的值大于基准值则与右指针进行交换，同时右指针向左移动；否则左指针继续向右移动。
+
+partition： begin=0, end=9, array=[9, 7, 5, 11, 12, 2, 14, 3, 10, 6]
+Pivot=9
+left index=3 right index=9 （right index指向8）
+交换index=3和index=9，array=[9, 7, 5, 6, 12, 2, 14, 3, 10,11]
+
+left index=4 right index=8 （right index指向7）
+交换index=3和index=8，array=[9, 7, 5, 6, 10, 2, 14, 3, 12,11]
+
+left index=4 right index=7 （right index指向6）
+交换index=4和index=7，array=[9, 7, 5, 6, 3, 2, 14, 10, 12,11]
+
+left index=6 right index=6 终止扫描
+将基准元素与中间元素进行交换（如果中间元素大于基准元素，则交换前一个元素），array=[2, 7, 5, 6, 3, 9, 14, 10, 12,11]
+
+left 2, 7, 5, 6, 3
+Pivot=2
+left index=1 right index=4 （right index指向3）
+交换index=1和index=4，array=[2, 3, 5, 6, 7, 9, 14, 10, 12,11]
+
+left index=1 right index=3 （right index指向2）
+交换index=1和index=3，array=[2, 6, 5, 3, 7, 9, 14, 10, 12,11]
+
+left index=1 right index=2 （right index指向1）
+交换index=1和index=2，array=[2, 5, 6, 3, 7, 9, 14, 10, 12,11]
+
+left index=1 right index=1 终止扫描
+中间元素大于基准元素，但其前面一个元素就是基准元素，因此不需要交换，array=[2, 5, 6, 3, 7, 9, 14, 10, 12,11]
+继续执行下一轮递归，end向左移动，直到begin = end，左侧的数组排序完成array=[2,3, 5, 6 ,7 ,9, 14, 10, 12, 11]
+
+right 14, 10, 12, 11
+再执行另一个递归,直到右侧的数组排序完成array=[2, 3, 5 ,6 ,7, 9, 10 ,11, 12 ,14]
+*/
 func partition(array []int, begin, end int) int {
 	i := begin + 1 // 将array[begin]作为基准数，因此从array[begin+1]开始与基准数比较！
 	j := end       // array[end]是数组的最后一位
